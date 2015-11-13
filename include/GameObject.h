a5c4b2317993f2dd61be85e2ec78afa3b94192bb
RossLGCAL <RLANGM200@caledonian.ac.uk>
2015-11-13 13:00:57 +0000

    Just renderibng game object

--- include/GameObject.h
+++ include/GameObject.h
@@ -2,17 +2,47 @@
 #define _GAMEOBJECT_H
 
 #include "Common.h"
-
+#include "Vertices.h"
+//google camel case
 class GameObject
 {
 public:
+
 	GameObject();
 	~GameObject();
 
-	void addChild(shared_ptr<GameObject> child);
-private:
-	vector<shared_ptr<GameObject> > m_ChildObjects;
-};
+	void update();
 
+	void createBuffer(Vertex *pVerts,int numVerts,
+		int *pIndices,int numIndices);
+
+	void loadShader(const string& vsFilename, const string& fsFilename);
+
+	GLuint getShaderProgram()
+	{
+		return m_ShaderProgram;
+	};
+
+private:
+	GLuint m_VBO;
+	GLuint m_EBO;
+	GLuint m_VAO;
+	GLuint m_shaderProgram;
+	int m_NoOfVertices;
+	int m_NoOfIndices;
+
+	mat4 m_ModeldMatrix;
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+	vec4 m_AmbientMaterial;
+	vec4 m_DiffuseMaterial;
+	vec4 m_SpecularMaterial;
+	float m_SpecularPower;
+protected:
+
+
+};
 
 #endif
